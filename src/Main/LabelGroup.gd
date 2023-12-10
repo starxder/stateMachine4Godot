3dcf4d54b844e9a43346ef9a600661acdec6cf60
starxder <starxder@gmail.com>
2023-12-10 23:47:17 +0800

    save

--- src/Main/LabelGroup.gd
+++ src/Main/LabelGroup.gd
@@ -4,5 +4,7 @@ func _on_state_machine_transitioned(state_path) -> void:
 	var node = get_child(0)
 	#text = state_path
 	print_rich(state_path)
+	var child = get_child(0)
+	child.text = state_path
 	pass # Replace with function body.
  
